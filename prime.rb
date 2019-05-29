168b74d2a7367c0be27f1401e2ae785276b1407b
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-29 21:21:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,4 @@
-// def prime?(n)
+#! def prime?(n)
   if n == 2 || n == 3 || n == 5
     return true
   elsif n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0
@@ -8,7 +8,7 @@
   elsif n % -2 == 0 || n % -3 == 0 || n % -5 == 0 || n % -7 == 0
     return false
   end
-end##
+end
 
 def isprime(n):
     """Returns True if n is prime."""
