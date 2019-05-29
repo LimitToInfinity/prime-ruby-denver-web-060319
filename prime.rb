5e62cc11d9f4d2879cc7f248696691d17aefd1de
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-29 21:50:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -14,6 +14,6 @@ end
 
 def is_prime?(num)
   return false if num <= 1
-  Math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
+  math.sqrt(num).to_i.downto(2).each {|i| return false if num % i == 0}
   true
 end
