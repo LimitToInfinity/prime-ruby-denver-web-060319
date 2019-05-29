9f965743653f5612f8eca0beafd6e714819a0b0c
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-29 21:19:33 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,5 +5,7 @@ def prime?(n)
     return false
   elsif n % 2 != 0 || n % 3 != 0 || n % 5 != 0 || n % 7 != 0
     return true
+  elsif n % -2 == 0 || n % -3 == 0 || n % -5 == 0 || n % -7 == 0
+    return false
   end
 end
