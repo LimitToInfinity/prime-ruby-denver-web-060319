bbe315b2f0496dfe408f6d61063c78156556583f
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-29 21:25:40 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,4 +1,3 @@
-=begin
 def prime?(n)
   if n == 2 || n == 3 || n == 5
     return true
@@ -8,30 +7,5 @@ def prime?(n)
     return true
   elsif n % -2 == 0 || n % -3 == 0 || n % -5 == 0 || n % -7 == 0
     return false
-  end
-end
-=end
-
-def prime?(n)
-  if n == 2
-    return true
-  elsif n == 3
-    return true
-  elsif n % 2 == 0
-    return false
-  elsif n % 3 == 0
-    return false
-  end
-
-  i = 5
-  w = 2
-
-  while i * i <= n
-    if n % i == 0
-      return false
-        i += w
-        w = 6 - w
-    return true
-    end
   end
 end
