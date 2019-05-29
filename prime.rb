9affbbf8dff0a200efcbe60b2144d7a41c7ded80
Ahmed Gaber <21stlimitless@gmail.com>
2019-05-29 21:59:58 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 def prime?(n)
-  if n >= -1 || n <= 1
-    return false
-  elsif n == 2 || n == 3 || n == 5
+  if n == 2 || n == 3 || n == 5
     return true
+  elsif n >= -1 || n <= 1
+    return false
   elsif n % 2 == 0 || n % 3 == 0 || n % 5 == 0 || n % 7 == 0
     return false
   elsif n % 2 != 0 || n % 3 != 0 || n % 5 != 0 || n % 7 != 0
